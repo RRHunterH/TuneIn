39d8ccc61ea7a55c3e07acd8a01b2d9fc63da95b
kristie fisher <fisherkristie19@icloud.com>
2024-05-02 19:18:08 -0400

    added second search for events with the Ticketmaster API

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -5,17 +5,33 @@ import SearchResult from './SearchResult';
 function MainSection() {
   const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearch = (query) => {
+  const [secondSearchResults, setSecondSearchResults] = useState([]);
+
+  const handleLyricsSearch = (query) => {
     fetch(`https://api.genius.com/search?q=${encodeURIComponent(query)}&access_token=9xRbA32eXhmEx1y8PiT6ilBROAS6qe-6ymKJmCetD5KRXKtXYcngRt8uJh0k3C1O`)
       .then(response => response.json())
       .then(data => setSearchResults(data.response.hits))
       .catch(error => console.error("Error fetching search results:", error));
   };
 
+  const handleEventsSearch = (query) => {
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&keyword=${encodeURIComponent(artist)}`, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => setSecondSearchResults(data._embedded.events)) 
+    .catch(error => console.error("Error fetching search results:", error));
+  };
+
   return (
     <div>
-      <Search onSearch={handleSearch} />
+      <h1>Search Lyrics</h1>
+      <Search onSearch={handleLyricsSearch} />
       <SearchResult results={searchResults} />
+
+      <h1>Search Events</h1>
+      <Search onSearch={handleEventsSearch} />
+      <SearchResult results={secondSearchResults} />
     </div>
   );
 }
