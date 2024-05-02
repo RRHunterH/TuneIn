48290dcd484791fbf6a8e71d696fe36a8060c266
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-05-02 02:19:16 -0400

    added Layout for App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import MainSection from './MainSection';
+import Footer from './Footer';
+import './App.css'; //Place holder until I add the App.css
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
